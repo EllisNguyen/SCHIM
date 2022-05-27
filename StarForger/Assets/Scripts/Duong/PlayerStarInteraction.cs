57c606acaedab9fa1aff8c3c338321a7ca0b7560
Griffin <51525281+duong-lc@users.noreply.github.com>
2022-05-28 00:35:26 +0700

    Scriptable object ensurance

--- StarForger/Assets/Scripts/Duong/PlayerStarInteraction.cs
+++ StarForger/Assets/Scripts/Duong/PlayerStarInteraction.cs
@@ -1,18 +1,38 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public struct InvenItem
+{
+    public StarType starType;
+    public int starCount;
+}
+
 public class PlayerStarInteraction : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //private List<SO_StarPickupType> _starTypeList = new List<SO_StarPickupType>();
+    private List<InvenItem> _inventoryList = new List<InvenItem>();
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
+        var starTypeList = GameManager.Instance.starPickupTypeList;
+        
+        //initialize with size
+        _inventoryList = new List<InvenItem>(starTypeList.Count);
+        foreach (var item in _inventoryList)
+        {
+            
+        }
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        var starBehavior = other.GetComponent<StarBehavior>();
+        
+        if (!starBehavior) return;
+
+        var starValue = starBehavior.GetStarType();
         
     }
 }
