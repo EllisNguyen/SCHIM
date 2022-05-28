c03e6d5449e56a468ec7b7482af1bfd48b51438f
Phap Nguyen <EllisTheMiracle@gmail.com>
2022-05-28 10:25:40 +0700

    update UI + scene manager test

--- StarForger/Assets/Scripts/Menu/SceneManager.cs
+++ StarForger/Assets/Scripts/Menu/SceneManager.cs
@@ -5,15 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class SceneManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] List<string> sceneName;
+
+    public void LoadScene()
     {
-        
+        Application.LoadLevel(1);
     }
 }
