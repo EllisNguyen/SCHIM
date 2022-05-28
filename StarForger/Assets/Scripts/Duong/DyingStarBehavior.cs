f3389c813ef6e670d6ad54bde5b30148125de801
Griffin <51525281+duong-lc@users.noreply.github.com>
2022-05-28 14:02:42 +0700

    Win condition added, dying star and random complete, gameplay loop complete

--- StarForger/Assets/Scripts/Duong/DyingStarBehavior.cs
+++ StarForger/Assets/Scripts/Duong/DyingStarBehavior.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 [RequireComponent(typeof(RecipeRandomizer))]
@@ -14,21 +15,22 @@ using UnityEngine;
 
 public class DyingStarBehavior : MonoBehaviour
 {
-    private RecipeElement _currentRecipeDataList;
+   // private RecipeElement _currentRecipeDataList = new RecipeElement(){recipeElement = new List<RecipeInvenItem>()};
     private RecipeRandomizer _randomizerBehavior => GetComponent<RecipeRandomizer>();
     
     private void Start()
     {
         //populating list of starType and their count to push into the DyingStar
-        foreach (var data in GameManager.Instance.starPickupTypeList)
-        {
-            var element = new InvenItem
-            {
-                starData = data,
-                starCount = 0
-            };
-            _currentRecipeDataList.recipeElement.Add(element);
-        }
+        // foreach (var data in GameManager.Instance.starPickupTypeList)
+        // {
+        //     var element = new RecipeInvenItem
+        //     {
+        //         starData = data,
+        //         starCurrent = 0,
+        //         starRequired = 0
+        //     };
+        //     _currentRecipeDataList.recipeElement.Add(element);
+        // }
         
     }
     
@@ -37,19 +39,40 @@ public class DyingStarBehavior : MonoBehaviour
         var starBehavior = other.GetComponent<StarBehavior>();
         if (!starBehavior) return;
 
-        foreach (var data in _currentRecipeDataList.recipeElement)
+
+        foreach (var data in _randomizerBehavior.recipeArray[_randomizerBehavior.currentRecipeIndex].recipeElement)
         {
             if (starBehavior.starData.starValue == data.starData.starValue)
             {
-                data.starCount++;
+                data.starCurrent++;
+                //GameManager.Instance.UpdateCurrentNum(data);
             }
         }
 
-        _randomizerBehavior.GetCurrentRecipe();
+        _randomizerBehavior.CheckCompleteRecipe();
+        
+        starBehavior.OnCollected();
         
         
-        var header1 = _randomizerBehavior.GetCurrentRecipeCount();
-        var header2 = _randomizerBehavior.GetMaxRecipeCount();
-        ScreenSpaceUIManager.Instance.UpdateHeader(header1, header2);
+        
+        // foreach (var data in _currentRecipeDataList.recipeElement)
+        // {
+        //     if (starBehavior.starData.starValue == data.starData.starValue)
+        //     {
+        //         data.starCount++;
+        //         GameManager.Instance.UpdateCurrentNum(data);
+        //     }
+        // }
+        //
+        //
+        //
+        // _randomizerBehavior.GetCurrentRecipe();
+        //
+        //
+        // var header1 = _randomizerBehavior.GetCurrentRecipeCount();
+        // var header2 = _randomizerBehavior.GetMaxRecipeCount();
+        //ScreenSpaceUIManager.Instance.UpdateHeader(header1, header2);
     }
+
+   
 }
--- StarForger/Assets/Scripts/Duong/RecipeRandomizer.cs
+++ StarForger/Assets/Scripts/Duong/RecipeRandomizer.cs
@@ -2,72 +2,129 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 using Random = UnityEngine.Random;
 
+
+public class RecipeInvenItem
+{
+    public int starCurrent;
+    public int starRequired;
+    public SO_StarPickupType starData;
+}
 [Serializable]
 public struct RecipeElement
 {
-    public List<InvenItem> recipeElement;
+    public List<RecipeInvenItem> recipeElement;
 }
 public class RecipeRandomizer : MonoBehaviour
 {
+    public static RecipeRandomizer Instance;
     [SerializeField] private int _recipeAmount = 5;
     [SerializeField] private int _maxStarStack = 3;//max amount of same type star in a single recipe
-    [SerializeField] private RecipeElement _recipeElement;
-    [SerializeField] private RecipeElement [] _recipeArray;
+    [SerializeField] private RecipeElement _recipeElement = new RecipeElement();
+    [SerializeField] public RecipeElement [] recipeArray;
+    public bool isWon = false;
 
-    private int _currentRecipeIndex = 0;
+    public int currentRecipeIndex = 0;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        _recipeArray = new RecipeElement [_recipeAmount];
+        _recipeElement.recipeElement = new List<RecipeInvenItem>();
+        recipeArray = new RecipeElement [_recipeAmount];
         //int recipeListNum = _recipeAmount;
         var starTypeList = GameManager.Instance.starPickupTypeList;
         
         
         //Populate the Recipe list with the element
-        for (int i = 0; i < _recipeArray.Length; i++)
+        for (int i = 0; i < recipeArray.Length; i++)
         {
             int totalStar = 0;
             //Populate the element
             for (int j = 0; j < starTypeList.Count; j++)
             {
-                print($"{j}");
-                _recipeElement.recipeElement.Add(new InvenItem());
+                //print($"{j}");
+                _recipeElement.recipeElement.Add(new RecipeInvenItem());
                 _recipeElement.recipeElement[j].starData = starTypeList[j];
                 
                 //avoid all 3 element being 0
                 if (j == starTypeList.Count - 1 && totalStar == 0)
                 {
-                    _recipeElement.recipeElement[j].starCount = (int)Random.Range(1, _maxStarStack+1);
+                    _recipeElement.recipeElement[j].starRequired = (int)Random.Range(1, _maxStarStack+1);
                 }
                 else
                 {
-                    totalStar = _recipeElement.recipeElement[j].starCount = (int)Random.Range(0, _maxStarStack+1);
+                    totalStar = _recipeElement.recipeElement[j].starRequired = (int)Random.Range(0, _maxStarStack+1);
                 }
             }
             
             //add the element to list and clear it to make a new one
-            _recipeArray[i] = _recipeElement;
+            recipeArray[i] = _recipeElement;
             //_recipeElement.recipeElement.Clear();
-            _recipeElement.recipeElement = new List<InvenItem>();
+            _recipeElement.recipeElement = new List<RecipeInvenItem>();
         }
+        
+        // //update required num
+        // foreach (var inventItems in _recipeArray[_currentRecipeIndex].recipeElement)
+        // {
+        //     GameManager.Instance.UpdateRequiredNum(inventItems);
+        // }
+        // //update recipe amount
+        // GameManager.Instance.UpdateCurrentRecipeCount(_currentRecipeIndex, _recipeAmount);
     }
 
-    public RecipeElement GetCurrentRecipe()
-    {
-        return new RecipeElement();
-    }
+    // public RecipeElement GetCurrentRecipe()
+    // {
+    //     return new RecipeElement();
+    // }
     
     public void CycleNextRecipe()
     {
+        currentRecipeIndex++;
+        
+        if (currentRecipeIndex > _recipeAmount - 1)
+        {
+            currentRecipeIndex--;
+            Debug.Log("WIN");
+            isWon = true;
+            return;
+        }
+        
+        // foreach (var inventItems in _recipeArray[_currentRecipeIndex].recipeElement)
+        // {
+        //     GameManager.Instance.UpdateRequiredNum(inventItems);
+        // }
+        //
+        // GameManager.Instance.UpdateCurrentRecipeCount(_currentRecipeIndex, _recipeAmount);
         
     }
 
+    public void CheckCompleteRecipe()
+    {
+        bool complete = true;
+        foreach (var data in recipeArray[currentRecipeIndex].recipeElement)
+        {
+            if (data.starCurrent < data.starRequired)
+            {
+                complete = false;
+            }
+        }
+
+        if (complete)
+        {
+            CycleNextRecipe();
+        }
+    }
+    
     public int GetCurrentRecipeCount()
     {
-        return _currentRecipeIndex;
+        return currentRecipeIndex;
     }
 
     public int GetMaxRecipeCount()
