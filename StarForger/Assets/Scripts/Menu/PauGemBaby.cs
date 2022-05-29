b2b2fa4708c6c46257da01e50bd9f8f265ff6e4d
Griffin <51525281+duong-lc@users.noreply.github.com>
2022-05-29 13:53:19 +0700

    update

--- StarForger/Assets/Scripts/Menu/PauGemBaby.cs
+++ StarForger/Assets/Scripts/Menu/PauGemBaby.cs
@@ -8,12 +8,22 @@ public class PauGemBaby : MonoBehaviour
     [SerializeField] GameObject pauseMenu;
     [SerializeField] bool isPaused = false;
 
+    public GameObject config;
+    public GameObject tutorial;
+    
     public void PauseGame(bool _isPaused)
     {
         if (_isPaused)
+        {
             Time.timeScale = 0;
+            
+        }
         else
+        {
+            config.SetActive(false);
+            tutorial.SetActive(false);
             Time.timeScale = 1;
+        }
         
         pauseMenu.SetActive(_isPaused);
     }
